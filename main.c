8ae98a1d07c53054059fe4e2c6fb41e6624a16c8
GiladFisher <giladfisher98@gmail.com>
2022-11-25 16:59:12 +0200

    Basic main template

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <"my_mat.h">
+#include <stdio.h>
+int main(){
+    char c = ' ';
+    while(c != 'D'){
+        scanf("%c", &c);
+        if (c == 'A'){
+            printf("go into first method");
+        }
+        else if (c == 'B'){
+            printf("go into second method");
+        }
+        else if (c == 'C'){
+            printf("go into third method");
+        }
+    }
+    return 0;
+}
+
+# Path: /mnt/c/Users/Selfisher/Documents/GitHub/SP2
