460ab811c8d97a9455b1ddfaeddb007b70a27193
BarakFinkel <barak.finkel11@gmail.com>
2022-11-28 15:48:47 +0200

    Update my_mat.c
    added routeExist and initBoolMat

--- my_mat.c
+++ my_mat.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-
 //The function used for initializing the matrice:
 
-int initMat(int *mat, int length)
+int initMat(int mat[4][4])
 {
     
     for(int i=0; i<4; i++ ){
@@ -32,7 +31,33 @@ int initMat(int *mat, int length)
     return 0;
 }
 
-int routeExist(int mat[][])
+int initBoolMat(int bmat[4][4])
 {
+    for(int i=0; i<4; i++){
+        
+        for(int j=0; i<4; i++){
 
+            bmat[i][j] = 0;
+        }
+    }
+
+
+    return 0;
+}
+
+int routeExist(int mat[4][4], int bmat[4][4],int i, int j)
+{
+    if( mat[i][j] != 0) {return 1;}
+
+    for(int k=0; k < 4; k++ )
+    {
+        if (k==i) {continue;}
+        if ( (mat[i][k] != 0) && (bmat[i][k] == 0) ) {
+            
+            bmat[i][k] = 1;
+            routeExist(mat, bmat, k, j);
+        }
+    }
+
+    return 0;
 }
